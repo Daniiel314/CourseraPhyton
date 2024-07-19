7d1a45f45bc5184b7b801710d96e692b3fb9fc24
Daniiel314 <36395002+Daniiel314@users.noreply.github.com>
2024-07-18 23:42:14 -0600

    end

---
+++ opengeo/where.js
@@ -0,0 +1,18 @@
+myData = [
+[50.065703299999996,19.918958667058632, 'AGH University of Science and Technology'],
+[52.2394966,21.015693129786936, 'Academy of Fine Arts in Warsaw'],
+[30.04281175,31.23658590813949, 'American University in Cairo'],
+[33.4213174,-111.93316305413154, 'Arizona State University'],
+[37.9425172,23.8706149, 'Athens Information Technology'],
+[17.547185,78.572791, 'BITS Pilani'],
+[39.1513078,-84.47083471474068, 'Xavier University'],
+[30.588607449999998,31.48192973657335, 'Zagazig University'],
+[33.6842062,73.0555742, 'Allama Iqbal Open University'],
+[33.4213174,-111.93316305413154, 'Arizona State University'],
+[35.627454,-99.385053, 'Arizona State University'],
+[39.20883,-96.58845116318602, 'Kansas State University'],
+[40.4314742,-3.7900439484094033, 'Universidad Complutense de Madrid (Campus de Somosaguas)'],
+[38.2187045,21.747894895431557, 'University of Patras'],
+[45.41667,11.86667, 'University of Padua'],
+[29.41972655,-98.48517444228465, 'Universidad Nacional Autonoma de Mexico (UNAM) - USA']
+];
